3e06b7e179f9f359a0df89b07098cd50fe67beff
Lukundo Kileha <lkileha@gmail.com>
2020-12-25 19:26:31 +0300

    added extra files to be added to dist

--- webpack.common.js
+++ webpack.common.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
   entry: './src/index.js',
@@ -8,5 +9,12 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     libraryExport: "default",
     libraryTarget: 'umd'
-  },
+  },  
+  plugins: [
+    new CopyPlugin([
+      { from: path.resolve(__dirname,'node_modules/h5p-standalone-fix/dist/styles'), to: path.resolve(__dirname,'dist/styles') },
+      { from: path.resolve(__dirname,'node_modules/h5p-standalone-fix/dist/fonts'), to: path.resolve(__dirname,'dist/fonts') },
+      { from: path.resolve(__dirname,'node_modules/h5p-standalone-fix/dist/frame.bundle.js'), to: path.resolve(__dirname,'dist/frame.bundle.js') }
+    ]),
+  ]
 };
